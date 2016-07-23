acef65e83e000fdcf8db6de181460657154edde8
Brian Yu <brianyu28@gmail.com>
2016-07-23 14:29:35 -0400

    added distinction between opinion and news quote cards

--- quotecard.py
+++ quotecard.py
@@ -11,30 +11,50 @@ position = raw_input('Position: ')
 opinion = raw_input('Opinion? (y/n): ') in ['y', 'yes', 'YES', 'Y', 'Yes', '1']
 
 
-img = Image.new('RGB', (1120, 600), "#a82931")
+
 quotetext_font = ImageFont.truetype("suecaslab-light.otf", 60)
 authortext_font = ImageFont.truetype("suecaslab-heavy.otf", 60)
-titletext_font = ImageFont.truetype("suecaslab-light.otf", 63)
-opinion_font = ImageFont.truetype("suecaslab-semibold.otf", 55)
-thc_font = ImageFont.truetype("bigmoore.otf", 18)
+titletext_font = ImageFont.truetype("suecaslab-light.otf", 45)
+opinion_font = ImageFont.truetype("suecaslab-semibold.otf", 60)
+thc_op_font = ImageFont.truetype("bigmoore.otf", 18)
+thc_font = ImageFont.truetype("bigmoore.otf", 36)
 
 breakquote = textwrap.wrap(quote, width=35)
 
-draw = ImageDraw.Draw(img)
 
-counter = 0
-for line in breakquote:
-    draw.text((28,50 + (counter * 60)), line, font=quotetext_font)
-    counter += 1
-    
-draw.text((28, 420), author, font=authortext_font)
-draw.text((28, 490), position, font=titletext_font)
+img = None
 
 if opinion:
-    draw.text((900, 490), "opinion", font=opinion_font)
-    draw.text((910, 530), "The Harvard Crimson", font=thc_font)
+    img = Image.new('RGB', (1120, 600), "#a82931")
+    draw = ImageDraw.Draw(img)
+    counter = 0
+    for line in breakquote:
+        draw.text((28,50 + (counter * 60)), line, font=quotetext_font, fill=(255,255,255,255))
+        counter += 1
+    
+    draw.text((28, 420), author, font=authortext_font, fill=(255,255,255,255))
+    draw.text((28, 490), position, font=titletext_font, fill=(255,255,255,255))
+        
+    draw.text((860, 510), "opinion", font=opinion_font, fill=(255,255,255,255))
+    draw.text((922, 570), "The Harvard Crimson", font=thc_op_font, fill=(255,255,255,255))
+    draw = ImageDraw.Draw(img)
 
-
-draw = ImageDraw.Draw(img)
+else:
+    seal = Image.open("seal-trans-sm.png")
+    img = Image.new('RGB', (1120,600), "white")
+    img.paste(seal, (800, 250), seal)
+    draw = ImageDraw.Draw(img)
+    draw.rectangle([(0,0), (1120, 30)], "#a82931")
+    
+    counter = 0
+    for line in breakquote:
+        draw.text((28,50 + (counter * 60)), line, font=quotetext_font, fill="#a82931")
+        draw.text((28, 420), author, font=authortext_font, fill="#a82931")
+        draw.text((28, 490), position, font=titletext_font, fill="#a82931")
+        draw.text((775, 550), "The Harvard Crimson", font=thc_font, fill="#a82931")
+        counter += 1
+    
+    
+    draw = ImageDraw.Draw(img)
 
 img.show()
