f43befdc23357da14bd8249c46f84b9ff4ad154b
Brian Yu <brianyu28@gmail.com>
2016-07-23 14:03:39 -0400

    basic non-opinion quotes working

--- quotecard.py
+++ quotecard.py
@@ -5,14 +5,36 @@ import textwrap
 #400 x 220 for Twitter
 #1120 x 600
 
-quote = raw_input('Input Quote: ')
+quote = raw_input('Quote: ')
+author = raw_input('Author: ')
+position = raw_input('Position: ')
+opinion = raw_input('Opinion? (y/n): ') in ['y', 'yes', 'YES', 'Y', 'Yes', '1']
 
-author = "DKC"
+
 img = Image.new('RGB', (1120, 600), "#a82931")
-sueca_light = ImageFont.truetype("suecaslab-light.otf", 24)
+quotetext_font = ImageFont.truetype("suecaslab-light.otf", 60)
+authortext_font = ImageFont.truetype("suecaslab-heavy.otf", 60)
+titletext_font = ImageFont.truetype("suecaslab-light.otf", 63)
+opinion_font = ImageFont.truetype("suecaslab-semibold.otf", 55)
+thc_font = ImageFont.truetype("bigmoore.otf", 18)
+
+breakquote = textwrap.wrap(quote, width=35)
 
 draw = ImageDraw.Draw(img)
-draw.text((0,0), quote, font=sueca_light)
+
+counter = 0
+for line in breakquote:
+    draw.text((28,50 + (counter * 60)), line, font=quotetext_font)
+    counter += 1
+    
+draw.text((28, 420), author, font=authortext_font)
+draw.text((28, 490), position, font=titletext_font)
+
+if opinion:
+    draw.text((900, 490), "opinion", font=opinion_font)
+    draw.text((910, 530), "The Harvard Crimson", font=thc_font)
+
+
 draw = ImageDraw.Draw(img)
 
 img.show()
