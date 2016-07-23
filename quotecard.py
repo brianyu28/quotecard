19e387f1eed13f0bce93f9264a59fcd54b72490e
Brian Yu <brianyu28@gmail.com>
2016-07-23 14:30:53 -0400

    style changes

--- quotecard.py
+++ quotecard.py
@@ -1,16 +1,10 @@
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
 
-#1200 x 627 for Facebook
-#400 x 220 for Twitter
-#1120 x 600
-
 quote = raw_input('Quote: ')
 author = raw_input('Author: ')
 position = raw_input('Position: ')
 opinion = raw_input('Opinion? (y/n): ') in ['y', 'yes', 'YES', 'Y', 'Yes', '1']
-
-
 
 quotetext_font = ImageFont.truetype("suecaslab-light.otf", 60)
 authortext_font = ImageFont.truetype("suecaslab-heavy.otf", 60)
@@ -21,9 +15,7 @@ thc_font = ImageFont.truetype("bigmoore.otf", 36)
 
 breakquote = textwrap.wrap(quote, width=35)
 
-
 img = None
-
 if opinion:
     img = Image.new('RGB', (1120, 600), "#a82931")
     draw = ImageDraw.Draw(img)
@@ -31,21 +23,18 @@ if opinion:
     for line in breakquote:
         draw.text((28,50 + (counter * 60)), line, font=quotetext_font, fill=(255,255,255,255))
         counter += 1
-    
     draw.text((28, 420), author, font=authortext_font, fill=(255,255,255,255))
     draw.text((28, 490), position, font=titletext_font, fill=(255,255,255,255))
         
     draw.text((860, 510), "opinion", font=opinion_font, fill=(255,255,255,255))
     draw.text((922, 570), "The Harvard Crimson", font=thc_op_font, fill=(255,255,255,255))
     draw = ImageDraw.Draw(img)
-
 else:
     seal = Image.open("seal-trans-sm.png")
     img = Image.new('RGB', (1120,600), "white")
     img.paste(seal, (800, 250), seal)
     draw = ImageDraw.Draw(img)
     draw.rectangle([(0,0), (1120, 30)], "#a82931")
-    
     counter = 0
     for line in breakquote:
         draw.text((28,50 + (counter * 60)), line, font=quotetext_font, fill="#a82931")
@@ -53,8 +42,6 @@ else:
         draw.text((28, 490), position, font=titletext_font, fill="#a82931")
         draw.text((775, 550), "The Harvard Crimson", font=thc_font, fill="#a82931")
         counter += 1
-    
-    
     draw = ImageDraw.Draw(img)
 
 img.show()
